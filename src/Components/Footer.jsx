bebe542ee38fccba8dddab31ef89c24c14cce9c4
Flávio Lins <35618051+Flavio13Lins@users.noreply.github.com>
2024-04-14 14:15:44 +0000

    refactor: replace copy icon from socials folder

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,7 +18,7 @@ import linkedInIcon from "../images/socials/linkedin.svg";
 import mediumIcon from "../images/socials/medium.svg";
 import twitterIcon from "../images/socials/twitter.svg";
 import youTubeIcon from "../images/socials/youtube.svg";
-import copyIcon from "../images/socials/copy.svg";
+import copyIcon from "../images/copy.svg";
 
 /**
  * 💡 Learning resources
