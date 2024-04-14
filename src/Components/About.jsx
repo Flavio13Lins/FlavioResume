1fc73ea006d80c7f608a2024c4f0829c801728d7
Flávio Lins <35618051+Flavio13Lins@users.noreply.github.com>
2024-04-14 14:27:21 +0000

    docs: changes on About component documentation

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,30 +6,13 @@
 
 import React from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/background.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Green, blue and yello abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
   "I am a graduated B.Sc. in Computer Engineering, I have been working with software development for over 5 years. I am a IBM Certified: Artificial Intelligence Practitioner.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
   "Analytical Thinking",
   "Detail-Oriented",
@@ -39,11 +22,6 @@ const skillsList = [
   "Easygoing",
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote =
   "I am passionate about technology, Cloud Computing, and building innovative solutions. I also help a students community to contribute in global hackathons events.";
 
